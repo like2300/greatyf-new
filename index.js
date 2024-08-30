b48e4055d954ed4ccbd3be5db0f83fdbd8d2c921
like2300 <elengaomerfils@gmail.com>
2024-08-30 17:40:17 +0100

    go2.3

--- index.js
+++ index.js
@@ -8,20 +8,30 @@ const chargement = () => {
 // mobile nav
 // Configuration de la navigation mobile
 const openNav = () => {
-  const btn = document.getElementById("btn");  // Utilisez "btn" ici
+  const btn = document.getElementById("btn");
   const mobileNav = document.getElementById("moblie_nav");
 
+  // Toggle le menu mobile lorsque l'utilisateur clique sur le bouton
   btn.addEventListener("click", () => {
     mobileNav.classList.toggle("translate-y-80");
     mobileNav.classList.toggle("translate-y-0");
+  });
 
+  // Fermer le menu mobile si l'utilisateur clique en dehors du menu ou du bouton
+  document.addEventListener("click", (event) => {
+    const isClickInsideNav = mobileNav.contains(event.target);
+    const isClickOnButton = btn.contains(event.target);
+
+    // Si le clic est à l'extérieur du menu et du bouton, on ferme le menu
+    if (!isClickInsideNav && !isClickOnButton) {
+      mobileNav.classList.add("translate-y-80"); // Cache le menu
+      mobileNav.classList.remove("translate-y-0"); // Assure que le menu est fermé
+    }
   });
 };
 
- 
 
 // slide fond for head page
-
 const slideFond = () => {
   const slides = document.querySelectorAll("#carousel div");
   let currentSlide = 0;
